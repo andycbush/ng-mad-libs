fb3e3ee7a6b340fa4406939df6dc97fb6111de0d
Andy Bush <andycbush@yahoo.com>
2016-07-25 21:59:03 -0400

    saving current work for v3

--- refactor/refactor.js
+++ refactor/refactor.js
@@ -1,7 +1,33 @@
-angular.module('myRefactor', [])
+var app = angular.module('myRefactor', ['ngMessages'])
+
+app.constant('VERSION', "1.3")
+app.run(function (VERSION, $rootScope) {
+    $rootScope.version = VERSION;
+})
+
+.controller("MyCtrl", function ($scope) {
+    $scope.data = {};
+
+    $scope.gender = {};
+
+    $scope.inputHide = false;
+
+    $scope.submit = function () {
+        $scope.inputHide = true;
+    }
+    $scope.reset = function () {
+        $scope.inputHide = false;
+        $scope.data = {};
+        location.reload();
+    }
+});
+
+
+/*angular.module('myRefactor', [])
     .controller('MyCtrl', function () {
         this.version = 1.2;
     })
     .controller('MyCtrlTitle', function () {
         this.title = 'Mad Libs Refactor 2 Challenge, Controller with Controller As';
     });
+*/
